09e8f1b4a3108e6de06a0ffca0b2a945dec24607
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-11-29 08:50:38 +0100

    CategoryController Updated

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,9 +1,8 @@
 class CategoryController < ApplicationController
-    
     def index
         @categories = Category.all
     end
-    
+
     def show
         @category = Category.find(params[:id])
         fresh_when etag: @category
@@ -12,20 +11,20 @@ class CategoryController < ApplicationController
     # def new
     #     category = Category.new
     # end
-    
+
     def create
        @category = Category.create!(category_params)
        redirect_to category
     end
-    
+
     def update
        @category = Category.find(params[:id])
     end
-    
+
     def edit
        @category = Category.find(params[:id])
     end
-    
+
     def destroy
        category = Category.find(params[:id])
     end
@@ -34,5 +33,4 @@ class CategoryController < ApplicationController
       def category_params
         params.require(:category).permit(:title, :description, :image)
       end
-        
 end
